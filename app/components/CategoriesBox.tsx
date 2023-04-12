218d8135ce53072ac341dd9fdee0588cdd72b810
Emmanuel Keifala <54929076+EmmanuelKeifala@users.noreply.github.com>
2023-04-12 10:11:45 +0000

    Update CategoriesBox.tsx

--- app/components/CategoriesBox.tsx
+++ app/components/CategoriesBox.tsx
@@ -1,53 +1,53 @@
-/** @format */
+'use client';
 
-"use client";
-import { IconType } from "react-icons";
+import qs from 'query-string';
 import { useRouter, useSearchParams } from "next/navigation";
 import { useCallback } from "react";
-import qs from "query-string";
+import { IconType } from "react-icons";
 
 interface CategoryBoxProps {
-	icon: IconType;
-	label: string;
-	selected?: boolean;
+  icon: IconType,
+  label: string;
+  selected?: boolean;
 }
+
 const CategoryBox: React.FC<CategoryBoxProps> = ({
-	icon: Icon,
-	label,
-	selected,
+  icon: Icon,
+  label,
+  selected,
 }) => {
-	const router = useRouter();
-	const params = useSearchParams();
+  const router = useRouter();
+  const params = useSearchParams();
 
-	const handleClick = useCallback(() => {
-		let currentQuery = [];
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    
+    if (params) {
+      currentQuery = qs.parse(params.toString())
+    }
 
-		if (params) {
-			currentQuery = qs.parse(params.toString());
-		}
-		const updatedQuery: any = {
-			...currentQuery,
-			category: label,
-		};
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label
+    }
 
-		if (params?.get("category") === label) {
-			delete updatedQuery.category;
-		}
+    if (params?.get('category') === label) {
+      delete updatedQuery.category;
+    }
 
-		const url = qs.stringifyUrl(
-			{
-				url: "/",
-				query: updatedQuery,
-			},
-			{ skipNull: true },
-		);
+    const url = qs.stringifyUrl({
+      url: '/',
+      query: updatedQuery
+    }, { skipNull: true });
 
-		router.push(url);
-	}, [label, params, router]);
-	return (
-		<div
-			onClick={handleClick}
-			className={`flex 
+    router.push(url);
+  }, [label, router, params]);
+
+  return ( 
+    <div
+      onClick={handleClick}
+      className={`
+        flex 
         flex-col 
         items-center 
         justify-center 
@@ -56,14 +56,17 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
         border-b-2
         hover:text-neutral-800
         transition
-        cursor-pointer    ${
-					selected ? "border-b-neutral-800" : "border-transparent"
-				}
-        ${selected ? "text-neutral-800" : "text-neutral-500"}`}>
-			<Icon size={26} />
-			<div className="font-medium text-sm">{label}</div>
-		</div>
-	);
-};
-
+        cursor-pointer
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
+      `}
+    >
+      <Icon size={26} />
+      <div className="font-medium text-sm">
+        {label}
+      </div>
+    </div>
+   );
+}
+ 
 export default CategoryBox;
